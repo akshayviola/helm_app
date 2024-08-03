eb47262d6e4db12e59f5f5231fcf6c6483bd6426
akshayviola <akshayviola201@gmail.com>
2024-08-03 18:41:01 +0530

    Modified index.js and value.yaml

--- nodejs-app/index.js
+++ nodejs-app/index.js
@@ -3,7 +3,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-  res.send('it works!!!');
+  res.send('ta da it works');
 });
 
 app.listen(port, () => {
